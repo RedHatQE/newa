9e0c75e4b00a2fc09cfe4e060db3eb0a95794706
Jesus Checa Hidalgo <jchecahi@redhat.com>
2024-03-20 09:33:34 +0100

    Add testsuite
    For now there is not much logic to test in the project so we are only
    adding the needed configuration in the pyproject.toml and some of
    smoke unit tests.

---
+++ tests/unit/test_render_template.py
@@ -0,0 +1,22 @@
+import pytest
+
+from newa import render_template
+
+
+@pytest.fixture()
+def simple_template():
+    return "simple template which replaces {{ TESTVAR }}"
+
+
+def test_render_template_good(simple_template):
+    rendered = render_template(simple_template, TESTVAR='something')
+    print(rendered)
+    assert 'something' in rendered
+
+
+def test_render_template_bad():
+    # Render template with (intentional) typo and ensures it raises
+    with pytest.raises(Exception, match='Could not parse template'):
+        render_template(
+            "{% if %} {% ednif %}",
+            )
