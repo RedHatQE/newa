7b90ba8a251132b081be35d8ff5870f8e25de1cb
Jesus Checa Hidalgo <jchecahi@redhat.com>
2024-04-17 11:19:20 +0200

    Add simple test for RecipeConfig class

---
+++ tests/unit/test_recipe.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from newa import RecipeConfig
+
+
+def test_recipeconfig_ok():
+    config = RecipeConfig.from_yaml_file(Path('tests/unit/data/sample_recipe.yaml').absolute())
+    reqs = list(config.build_requests())
+
+    # Check generated requests are correct
+    assert len(reqs) == 4
+    assert all('arch' in r.context for r in reqs)
+    assert all('fips' in r.context for r in reqs)
+    assert all('FIPS' in r.environment for r in reqs)
+    # Assert recipe id uniqueness
+    assert len(reqs) == len({r.id for r in reqs})
